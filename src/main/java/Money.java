7a978da25f0f0e9fd06d6ec61f6603764455d53e
javaRus <ruslan.bur@gmail.com>
2016-09-24 13:19:27 +0300

    First commit

---
+++ src/main/java/Money.java
@@ -0,0 +1,47 @@
+public class Money implements Expression {
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    static Money frank(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+            && currency().equals(money.currency());
+    }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    protected String currency() {
+        return currency;
+    }
+
+    Expression plus(Money added) {
+        return new Sum(this, added);
+    }
+
+    public Money reduce(String to) {
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+            "amount=" + amount +
+            ", currency='" + currency + '\'' +
+            '}';
+    }
+}

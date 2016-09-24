d1b80f929fbef6781688818bbd729f869200c9eb
javaRus <ruslan.bur@gmail.com>
2016-09-24 16:16:35 +0300

    Обмен валюты

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,8 +1,6 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class MoneyTest {
 
@@ -57,5 +55,29 @@ public class MoneyTest {
         Bank bank = new Bank();
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.frank(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testArrayEquals() {
+        // in book this don't work
+        assertEquals(new Object[]{"abc"}, new Object[]{"abc"});
+    }
+
+    @Test
+    public void testArrayEquals2() {
+        assertArrayEquals(new Object[]{"abc"}, new Object[]{"abc"});
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
     }
 }
